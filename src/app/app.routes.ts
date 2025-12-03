6d7df3c44db54396ba0da48be687d3d3765b7b45
cd28dev <carlos.adrianzen27972@gmail.com>
2025-12-02 19:36:55 -0500

    chore: actualizar rutas, main.ts y estilos globales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent} from './features/home/home.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { FormacionComponent } from './features/formacion/formacion.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'formacion', component: FormacionComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' }
+];
